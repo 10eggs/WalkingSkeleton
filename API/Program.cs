decb6755c051f9b8725fa88b4ce09764378a85da
10eggs <pawlak325@gmail.com>
2022-12-05 16:47:27 +0100

    Initial configuration

--- API/Program.cs
+++ API/Program.cs
@@ -3,46 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Persistance;
-using Persistence;
 
-//CTRL+.
 namespace API
 {
-  public class Program
-  {
-    public static async Task Main(string[] args)
+    public class Program
     {
-      var host = CreateHostBuilder(args).Build();
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-      using var scope = host.Services.CreateScope();
-
-      var services = scope.ServiceProvider;
-
-      try
-      {
-        var context = services.GetRequiredService<DataContext>();
-        await context.Database.MigrateAsync();
-
-        await Seed.SeedData(context);
-      }
-      catch (Exception ex)
-      {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occured during migration");
-      }
-      await host.RunAsync();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -6,24 +6,25 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
 using Persistance;
 
 namespace API
 {
   public class Startup
   {
-    private readonly IConfiguration _config;
-    public Startup(IConfiguration config)
+    private readonly IConfiguration _config
+        public Startup(IConfiguration config)
     {
       _config = config;
     }
 
+    // public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
@@ -38,9 +39,7 @@ namespace API
       services.AddDbContext<DataContext>(opt =>
       {
         opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-
       });
-
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,8 +52,7 @@ namespace API
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
       }
 
-
-      app.UseHttpsRedirection();
+      // app.UseHttpsRedirection();
 
       app.UseRouting();
 
