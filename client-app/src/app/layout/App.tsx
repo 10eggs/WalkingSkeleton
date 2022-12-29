cbe9ad42a67d042acb7ad3b9e4b3dd2bf5a2996f
10eggs <pawlak325@gmail.com>
2022-12-29 10:13:46 +0100

    React router introduced

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,14 @@
-import React, { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../features/activities/dashboard/ActivityDashboard';
-import agent from '../api/agent';
-import LoadingComponent from './LoadingComponents';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const {activityStore} = useStore();
-
-  useEffect(()=>{
-    activityStore.loadActivities();
-  },[activityStore]);
-
-
-  if(activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
-
   return (
     <>
       <NavBar/>
       <Container style ={{margin: '7em'}}>
-        <ActivityDashboard/>
+        <Outlet/>
       </Container>
     </>
   );
