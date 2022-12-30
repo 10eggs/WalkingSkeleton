2041e5b4c2793b65c62b1b321d715d315d9b80ac
10eggs <pawlak325@gmail.com>
2022-12-30 11:32:35 +0100

    Cleaning up

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -8,11 +8,12 @@ import ActivityList from './ActivityList';
 export default observer(function ActivityDashboard() {
 
   const {activityStore} = useStore();
+  const {loadActivities, activityRegistry} = activityStore;
   const {selectedActivity, editMode} = activityStore;
 
   useEffect(()=>{
-    activityStore.loadActivities();
-  },[activityStore]);
+    if(activityRegistry.size <= 1) loadActivities();
+  },[loadActivities,activityRegistry.size]);
 
 
   if(activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
