691da5686c88682ba3aae79d4edd3b81b9df4ee0
10eggs <pawlak325@gmail.com>
2022-12-30 11:07:36 +0100

    Moving the homepage outside navbar

--- client-app/src/app/features/home/HomePage.tsx
+++ client-app/src/app/features/home/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
 export default function HomePage(){
@@ -6,6 +7,9 @@ export default function HomePage(){
       <h1>
         Home Page
       </h1>
+      <h3>
+        Go to <Link to='/activities'>Activities</Link>
+      </h3>
     </Container>
   )
 }
