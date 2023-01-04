f71b3a3e473ace5a37b6dfed826cffd3df5af087
10eggs <pawlak325@gmail.com>
2023-01-04 17:25:27 +0100

    Introducing react-datepicker, custom datepicker created

--- client-app/src/app/features/activities/form/ActivityForm.tsx
+++ client-app/src/app/features/activities/form/ActivityForm.tsx
@@ -9,6 +9,10 @@ import {v4 as uuid} from 'uuid';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../../common/form/MyTextInput';
+import MyTextArea from '../../../common/form/MyTextArea';
+import MySelectInput from '../../../common/form/MySelectInput';
+import { categoryOptions } from '../../../common/options/CategoryOptions';
+import MyDateInput from '../../../common/form/MyDateInput';
 
 
 export default observer(function ActivityForm(){
@@ -70,9 +74,9 @@ export default observer(function ActivityForm(){
         {({handleSubmit})=>(
           <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
             <MyTextInput name='title' placeholder='Title'/>
-            <MyTextInput placeholder='Description' name='description' />
-            <MyTextInput placeholder='Category' name='category' />
-            <MyTextInput placeholder='Date' name='date' />
+            <MyTextArea rows={3} placeholder='Description' name='description' />
+            <MySelectInput options={categoryOptions} placeholder='Category' name='category' />
+            <MyDateInput placeholderText='Date' name='date' showTimeSelect timeCaption='time' dateFormat='MMMM d, yyyy h:mm: aa' />
             <MyTextInput placeholder='City' name='city' />
             <MyTextInput placeholder='Venue' name='venue' />
             <Button loading={loading} floated='right' positive type='submit' content='Submit'/>
