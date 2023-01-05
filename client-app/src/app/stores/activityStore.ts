aefe84e3c24e1839becb8c9ed96dafbd39f31e06
10eggs <pawlak325@gmail.com>
2023-01-05 12:53:44 +0100

    Using Date-FNS

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -3,6 +3,7 @@ import agent from '../api/agent';
 import { Activity } from '../models/activity';
 import {v4 as uuid} from 'uuid';
 import { ObjectFlags } from 'typescript';
+import { format } from 'date-fns';
 
 export default class ActivityStore {
   activityRegistry = new Map<string, Activity>();
@@ -21,20 +22,21 @@ export default class ActivityStore {
   }
 
   private setActivity = (activity:Activity) =>{
-    activity.date = activity.date.split('T')[0];
+    // activity.date = activity.date.split('T')[0];
+    activity.date = new Date(activity.date!);
     //We are mutating state of activities here
     //It would be an anti pattern in ReduX
     this.activityRegistry.set(activity.id,activity);
   }
 
   get activitiesByDate(){
-    return Array.from(this.activityRegistry.values()).sort((a,b) => Date.parse(a.date) - Date.parse(b.date));
+    return Array.from(this.activityRegistry.values()).sort((a,b) => a.date!.getTime() - b.date!.getTime());
   }
 
   get groupedActivities(){
     return Object.entries(
       this.activitiesByDate.reduce((activities,activity)=>{
-        const date = activity.date;
+        const date = format(activity.date!, 'dd MMM yyyy');
         activities[date] = activities[date] ? [...activities[date],activity] : [activity]
         return activities;
       }, {} as {[key: string]: Activity[]})
