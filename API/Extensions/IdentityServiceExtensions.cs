c5f5a81918b336b68483fe3ff5900f4f3ecfc182
10eggs <pawlak325@gmail.com>
2023-01-05 15:12:48 +0100

    Config for Identity, adding Identity to the services

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,21 @@
+using Domain;
+using Persistance;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityService(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt =>{
+                opt.Password.RequireNonAlphanumeric = false;
+
+            })
+            .AddEntityFrameworkStores<DataContext>();
+
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityService(builder.Configuration);
 
 var app = builder.Build();
 
