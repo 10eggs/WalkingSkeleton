3746a91fc0de8bdc9455d9d80a6afda426bb2f87
10eggs <pawlak325@gmail.com>
2023-01-08 15:57:52 +0100

    Interfaces for users added, agent request methods updated

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
+import { request } from 'http';
 import { toast } from 'react-toastify';
 import { Activity } from '../models/activity';
+import { User, UserFormValues } from '../models/user';
 import { router } from '../router/Routes';
 import { store } from '../stores/store';
 
@@ -71,8 +73,15 @@ const Activities = {
   delete: (id: string) => requests.del(`/activities/${id}`)
 };
 
+const Account = {
+  current: () => requests.get<User>('/account'),
+  login: (user: UserFormValues) => requests.post<User>('/account/login',user),
+  register: (user: UserFormValues) => requests.post<User>('/account/register',user)
+}
+
 const agent = {
-  Activities
+  Activities,
+  Account
 }
 
 export default agent;
